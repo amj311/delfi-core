53155dd20f8018ca7f529001be176008b8064216
Arthur Judd <amjudd315@gmail.com>
2022-04-20 00:03:45 -0600

    setup TS module basics

---
+++ README.md
@@ -0,0 +1,9 @@
+# Delfi Core
+
+This module is written in Typescript and should be bundled to JavaScript packages before using.
+
+The following command compiles the code using Webpack and the options in `tsconfig.json` and `webpack.config.js`.
+
+```
+$ npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const fs = require('fs');
+
+const SRC_DIR = path.resolve(__dirname, 'src/');
+const OUT_DIR = path.resolve(__dirname, 'dist');
+
+/**
+ * Gathers the handler functions files and returns an object for
+ * config.entry property
+ * 
+ * @returns object with pattern {...<name>:<path>}
+ */
+const handlers = () => {
+    let handlers = {};
+    fs.readdirSync(SRC_DIR).forEach(f => {
+        if (path.extname(f) == ".ts")
+            handlers[path.parse(f).name] = path.resolve(SRC_DIR, f);
+    });
+    return handlers;
+}
+
+const config = {
+    mode: "production",
+    entry: SRC_DIR+"/index.ts",
+    // aws-sdk is already available in the Node.js Lambda environment
+    //  so it should not be included in function bundles
+    externals: [
+        'aws-sdk'
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: OUT_DIR,
+        filename: 'index.js',
+        library: 'delfi',
+        libraryTarget: 'umd'
+    },
+    target: 'node'
+};
+
+module.exports = config;
